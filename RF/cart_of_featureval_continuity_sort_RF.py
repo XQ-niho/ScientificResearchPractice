b4c84dcc81b3dab747201811ccbad671f92718e9
XQ-niho <2252984586@qq.com>
2019-12-18 21:12:42 +0800

    随机分类预测结果以准确率为其判断好坏

--- RF/cart_of_featureval_continuity_sort_RF.py
+++ RF/cart_of_featureval_continuity_sort_RF.py
@@ -5,9 +5,9 @@ create on 2019-10
 @author: qiuzefeng
 """
 import csv
-import operator
-from numpy import  *
+from numpy import *
 from random import randrange
+import operator
 
 from show_matplotlip import creatPlot as cr
 
@@ -44,7 +44,7 @@ def ReadDataSet(csvfilename):
     labels = []
 
     with open(csvfilename) as csvfile:
-        #读取csvfile中的文件
+        # 读取csvfile中的文件
         csv_reader = csv.reader(csvfile)
         # 读取第一行每一列的标题
         label = next(csv_reader)
@@ -55,6 +55,31 @@ def ReadDataSet(csvfilename):
             dataSet.append(ro)
 
     return dataSet, labels
+
+def getTrainDataAndPredictData(dataSets):
+    """
+    把数据按7：3分成训练集与测试集
+    :param dataSet:
+    :return:
+    """
+    length = len(dataSets)
+
+    # 产生长度为length的序列
+    array = arange(length)
+    # 将序列随机排列
+    random.shuffle(array)
+    # array = selIndex
+
+    predictData = []
+    trainData = []
+
+    for index in array:
+        if len(trainData) <= length * 0.7:
+            trainData.append(dataSets[index])
+        else:
+            predictData.append(dataSets[index])
+
+    return trainData, predictData
 
 
 def calGini(dataSet):
@@ -137,7 +162,7 @@ def chooseBestFeatureToSplit(dataSet):
             GiniGain += right_prob * calGini(right_subDataSet)
 
             if (GiniGain <= bestGini):
-                #记录最好的结果和做好的特征
+                # 记录最好的结果和做好的特征
                 bestGini = GiniGain
                 bestFeature = i
                 bestBinaryFeatVal = value
@@ -159,7 +184,7 @@ def majorityCnt(classList):
 
     return sortedClassCount[0][0]
 
-def creatTree(dataSet, labels, m = 20, max_level = 10):
+def creatTree(dataSet, labels, max_level = 10):
     """
     构建CART树
     :param dataSet: 数据集
@@ -170,23 +195,26 @@ def creatTree(dataSet, labels, m = 20, max_level = 10):
     """
     classList = [example[-1] for example in dataSet]
 
+    # 如果所有类别都一样，建立节点
     if classList.count(classList[0]) == len(classList):
         return classList[0]
 
+    # 如果选择最佳的特征比不选的好则不选，直接建立节点
     if len(dataSet) == 1:
-        majorityCnt(classList)
+        return majorityCnt(classList)
 
     bestFeat, bestBinarySplitVal, bestGini = chooseBestFeatureToSplit(dataSet)
-    #如果所有特征Gini都一样，多数表决决定类别
+
+    # 如果所有特征Gini都比没划分时的阈值好，多数表决决定类别
     if bestFeat == -1:
         return majorityCnt(classList)
 
-    bestFeatLabel = labels[bestFeat]
+    bestFeatLabel = labels[bestFeat] + ':' + str(bestBinarySplitVal)
 
     myTree = {bestFeatLabel: {}}
 
     max_level -= 1
-    #如果深度小于设定深度，多数表决
+    # 如果深度小于设定深度，多数表决
     if max_level < 0:
         return majorityCnt(classList)
 
@@ -194,12 +222,28 @@ def creatTree(dataSet, labels, m = 20, max_level = 10):
     uniqueVals = list(set(featValues))
     subLabels = labels[:]  # 拷贝防止labels被修改
     del subLabels[bestFeat]
-    myTree[bestFeatLabel][bestFeatLabel + '<=' + str(round(float(bestBinarySplitVal), 3))] = creatTree(
-        splitDataSet(dataSet, bestFeat, bestBinarySplitVal, 'lt'), subLabels, m, max_level)
-    myTree[bestFeatLabel][bestFeatLabel + '>' + str(round(float(bestBinarySplitVal), 3))] = creatTree(
-        splitDataSet(dataSet, bestFeat, bestBinarySplitVal, 'gt'), subLabels, m, max_level)
+    myTree[bestFeatLabel]['<=' + str(round(float(bestBinarySplitVal), 3))] = creatTree(
+        splitDataSet(dataSet, bestFeat, bestBinarySplitVal, 'lt'), subLabels, max_level)
+    myTree[bestFeatLabel]['>' + str(round(float(bestBinarySplitVal), 3))] = creatTree(
+        splitDataSet(dataSet, bestFeat, bestBinarySplitVal, 'gt'), subLabels, max_level)
 
     return myTree
+
+def labelsDict(labels):
+    """
+    把特征与索引建立字典
+    :param labels:
+    :return:
+    """
+    length = len(labels)
+    labelsOfDict = {}
+
+    for i in range(length):
+        currentLabel = labels[i]
+        if currentLabel not in labelsOfDict.keys():
+            labelsOfDict[currentLabel] = i
+
+    return labelsOfDict
 
 def subsample(dataset):
     """
@@ -234,22 +278,91 @@ def RondomForest(dataSet, labels, n, max_level):
 
     return Trees
 
-def main():
-    path = "../datas/splice.csv"
+def predict(tree, predictData, labelsOfDict):
+    """
+    预测
+    :param tree: 决策树
+    :param sample: 预测数据
+    :param labelsOfDict: 特征字典索引
+    :return: 预测值
+    """
+    if type(tree) is not dict:
+        return tree
+    root = tree.keys()[0]
+    # 取出根节点，得到最优特征和阀值
+    feature, threshold = root.split(':')
+    threshold = float(threshold)
+    # 递归预测
+    if predictData[labelsOfDict[feature]] > threshold:
+        return predict(tree[root]['>' + str(round(float(threshold), 3))], predictData, labelsOfDict)
+    else:
+        return predict(tree[root]['<=' + str(round(float(threshold), 3))], predictData, labelsOfDict)
+
+def getPredictY(trees, predictDatas, labelsOfDict):
+    """
+    得到所有树的预测值
+    :param trees: 森林
+    :param samples: 测试集
+    :param predictLabelsDict: 特征索引字典
+    :return: 每棵树的Y
+    """
+    predictY = []
+
+    length = len(trees)
+    for samplerow in predictDatas:
+        predictAlltreeY = []
+        for i in range(length):
+            Y = predict(trees[i], samplerow, labelsOfDict)
+            predictAlltreeY.append(Y)
+        predictAlltreeMaxY = majorityCnt(predictAlltreeY)
+        predictY.append(predictAlltreeMaxY)
+
+    return predictY
+
+def correctRate(y, predictY):
+    """
+    计算预测的准确率
+    :param y: 预测集y
+    :param predictY: 预测结果
+    :return: 预测准确率
+    """
+    length = len(y)
+    count = 0
+
+    for index in range(length):
+        if y[index] == predictY[index]:
+            count += 1
+
+    corr = count / float(length)
+
+    return corr
+
+
+def result(path, tree_num, tree_depth):
+
     dataSet, labels = ReadDataSet(path)
-    print "{0}\n{1}".format(dataSet, labels)
 
-    Trees = RondomForest(dataSet, labels, 10, 6)
-    #输出每一颗决策树
-    # for i in range(10):
-    #     print Trees[i]
-        # cr.createPlot(Trees[i])
+    # 得到训练集与测试集
+    trainData, predictData = getTrainDataAndPredictData(dataSet)
 
+    # 得到预测集的类别
+    y = [example[-1] for example in predictData]
 
-    # myTree = creatTree(dataSet, labels, 5, 4)
-    # print myTree
-    # cr.createPlot(myTree)
+    # 产生森林 参数（训练集，特征，方差，树的个数，随机选择特征的个数，树的森度）
+    Trees = RondomForest(trainData, labels, tree_num, tree_depth)
 
+    # 预测 先把特征与索引建立字典为预测时找到对应的数据
+    labelsOfDict = labelsDict(labels)
+
+    # 得到每棵树的应变量的值 参数(森林，测试集，特征索引字典)
+    predictY = getPredictY(Trees, predictData, labelsOfDict)
+
+    # 得到准确率
+    corr = correctRate(y, predictY)
+
+    return corr
 
 if __name__ == '__main__':
-    main()
+    path = "../datas/splice.csv"
+    corr = result(path, 20, 15)
+    print corr
